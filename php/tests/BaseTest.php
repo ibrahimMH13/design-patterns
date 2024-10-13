76622630891fd8b83e09f8047fe29c58f510e7a7
Ibrahim II Musabeh <i.musabah92@gmail.com>
2024-10-13 10:18:00 +0800

    init php

---
+++ php/tests/BaseTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ibrahim13\PdPhp;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseTest extends TestCase
+{
+
+	#[TEST]
+	public function testIngration(){
+		$this->assertTrue(true);
+	}
+}
