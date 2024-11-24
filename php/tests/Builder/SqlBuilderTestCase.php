6e3c44c325c9143cda84e68cfe270d0a3e5abeff
Ibrahim II Musabeh <i.musabah92@gmail.com>
2024-11-24 12:39:23 +0800

    add another test

--- php/tests/Builder/SqlBuilderTestCase.php
+++ php/tests/Builder/SqlBuilderTestCase.php
@@ -6,6 +6,7 @@ namespace Ibrahim13\PdPhp\Test\Builder;
 
 use Ibrahim13\PdPhp\Test\BaseTest;
 use Ibrahim13\PdPhp\Builder\SQLBuilder\SqlBuilder;
+use Ibrahim13\PdPhp\Builder\SQLBuilder\SqlQuery;
 
 class SqlBuilderTestCase extends  BaseTest{
 
@@ -22,6 +23,20 @@ class SqlBuilderTestCase extends  BaseTest{
 						->getQuery();
 
 		$this->assertIsString($query);
+	}
+
+	public function testSqlQuery(){
+
+		$sql = new SqlQuery();
+
+		$sql->table = 'text';
+		$sql->fields = ['id','label'];
+		$sql->limit = 4;
+		$sql->orderBy ='desc';
+		$sql->conditions= ['id > 3'];
+		$this->assertIsString((string)$sql);	
+
+
 	}
 
 }
