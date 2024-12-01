ebdd180533c983a1e981357b8b67e6b3e5008474
Ibrahim II Musabeh <i.musabah92@gmail.com>
2024-12-01 14:25:10 +0800

    add sql builder,py

--- python/test/builder/test_sql_builder.py
+++ python/test/builder/test_sql_builder.py
@@ -1,14 +1,27 @@
 from builder.sql_query import SqlQuery
-
+from builder.sql_builder import SqlBuilder
 
 
 def test_sql_query():
 
    stmt = SqlQuery() 
-   stmt.table = "user"
+   stmt.table = "users"
    stmt.fields = ['id', 'first_name', 'last_name' , 'email', 'salary']
-   stmt.conditions = ["salary  > 5000"]
+   stmt.conditions = ["salary > 5000"]
    stmt.limit = 12
    stmt.orderBy ="id DESC"
 
-   assert str(stmt) == "SELECT id, first_name, last_name, email, salary FROM user WHERE salary  > 5000 LIMIT 12 ORDER BY id DESC"
+   assert str(stmt) == "SELECT id, first_name, last_name, email, salary FROM users WHERE salary > 5000 LIMIT 12 ORDER BY id DESC"
+
+
+
+def test_sql_builder():
+   
+   sql_builder = SqlBuilder()
+   sql = (sql_builder.select(['id','first_name','last_name','email','salary'])
+                    .from_table('users')
+                    .limit(5)
+                    .where(['salary > 5000'])
+                    .order_by('id','DESC'))
+  
+   assert str(sql.get_query()) == "SELECT id, first_name, last_name, email, salary FROM users WHERE salary > 5000 LIMIT 5 ORDER BY id DESC"
